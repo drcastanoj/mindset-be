04ac4b5846290d5cfdcbf8906d3b0c7daecd7548
MattHenao <matthenao10@gmail.com>
2023-04-27 11:28:00 -0500

    Not empty

--- src/appointment/appointment.dto.ts
+++ src/appointment/appointment.dto.ts
@@ -2,9 +2,13 @@ import { IsNotEmpty } from 'class-validator';
 
 export class AppointmentDto {
   userId: string;
+  @IsNotEmpty()
   day: number;
+  @IsNotEmpty()
   month: number;
+  @IsNotEmpty()
   year: number
+  @IsNotEmpty()
   hour: number;
   reason: string;
   @IsNotEmpty()
